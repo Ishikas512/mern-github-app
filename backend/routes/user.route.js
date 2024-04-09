e2093938798b2bfcc4bd1f22a6a134b1559a9526
Ishikas512 <ishikasarkar87640@gmail.com>
2024-04-09 15:14:46 +0530

    code refactor

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,9 +3,7 @@ import { getUserProfileAndRepos,likeProfile,getLikes } from "../controllers/user
 import {ensureAuthenticated} from "../middleware/ensureAuthenticated.js";
 
 const router=express.Router();
-router.get("/test", (req, res)=>{
-    res.json({message : "here in test!"});
-})
+
 router.get("/profile/:username",getUserProfileAndRepos);
 
 router.get("/likes",ensureAuthenticated,getLikes);
